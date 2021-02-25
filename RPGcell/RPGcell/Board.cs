6df234eb2134c73edb50cc8dc86588a0c1fceb2f
mamepuro <beans.mamegame.laugh@icloud.com>
2021-02-26 03:27:01 +0900

    BoardクラスからCellの選択をできないように変更

--- RPGcell/RPGcell/Board.cs
+++ RPGcell/RPGcell/Board.cs
@@ -11,7 +11,6 @@ namespace RPGcell
     class Board:SpriteNode
     {
         private const string textureResourcesPath = "Resources/Textures/";
-        private const int cellNumMax = 36;
         private const int rowMax = 6;
         private const int columnMax = 6;
         /// <summary>
@@ -34,55 +33,12 @@ namespace RPGcell
                     Engine.AddNode(cell);
                 }
             }
-            CelectingCellNumber = 1;
-            Cells[CelectingCellNumber].Texture = Texture2D.LoadStrict(textureResourcesPath +"selectedcell.png");
             var player = new Player(0, this);
             Engine.AddNode(player);
         }
 
-        private int CheckCellNumber(int cellNum)
-        {
-            cellNum = Math.Clamp(cellNum, 0, cellNumMax - 1);
-            return cellNum;
-        }
-
-        private void SelectCell(int cellNum)
-        {
-            //if(Engine.Keyboard.GetKeyState(Key.D) == ButtonState.Hold || Engine.Keyboard.GetKeyState(Key.Right) == ButtonState.Hold)
-            //{
-            //    Cells[CelectingCellNumber].Texture = Texture2D.LoadStrict(textureResourcesPath + "/cell.png");
-            //    CelectingCellNumber++;
-            //    CelectingCellNumber = CheckCellNumber(CelectingCellNumber);
-            //    Cells[CelectingCellNumber].Texture = Texture2D.LoadStrict(textureResourcesPath +"selectedcell.png");
-            //}
-            //if (Engine.Keyboard.GetKeyState(Key.A) == ButtonState.Hold || Engine.Keyboard.GetKeyState(Key.Left) == ButtonState.Hold)
-            //{
-            //    Cells[CelectingCellNumber].Texture = Texture2D.LoadStrict(textureResourcesPath+"cell.png");
-            //    //Cells[CelectingCellNumber].Position += new Vector2F(300, 0);
-            //    CelectingCellNumber--;
-            //    CelectingCellNumber = CheckCellNumber(CelectingCellNumber);
-            //    Cells[CelectingCellNumber].Texture = Texture2D.LoadStrict(textureResourcesPath +"selectedcell.png");
-            //}
-            //if (Engine.Keyboard.GetKeyState(Key.W) == ButtonState.Hold || Engine.Keyboard.GetKeyState(Key.Up) == ButtonState.Hold)
-            //{
-            //    Cells[CelectingCellNumber].Texture = Texture2D.LoadStrict(textureResourcesPath+"cell.png");
-            //    CelectingCellNumber -= 6;
-            //    CelectingCellNumber = CheckCellNumber(CelectingCellNumber);
-            //    Cells[CelectingCellNumber].Texture = Texture2D.LoadStrict(textureResourcesPath+"selectedcell.png");
-            //}
-            //if (Engine.Keyboard.GetKeyState(Key.S) == ButtonState.Hold || Engine.Keyboard.GetKeyState(Key.Down) == ButtonState.Hold)
-            //{
-            //    Cells[CelectingCellNumber].Texture = Texture2D.LoadStrict(textureResourcesPath+"cell.png");
-            //    //Cells[CelectingCellNumber].Position += new Vector2F(300, 0);
-            //    CelectingCellNumber += 6;
-            //    CelectingCellNumber = CheckCellNumber(CelectingCellNumber);
-            //    Cells[CelectingCellNumber].Texture = Texture2D.LoadStrict(textureResourcesPath+"selectedcell.png");
-            //}
-        }
-
         protected override void OnUpdate()
         {
-            //SelectCell(CelectingCellNumber);
             base.OnUpdate();
         }
     }
